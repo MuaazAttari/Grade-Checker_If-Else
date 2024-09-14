76110e6993a980af057a48b156b209068ed471d1
MuaazAttari <160422454+MuaazAttari@users.noreply.github.com>
2024-09-14 13:10:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+let grade = +prompt("Please enter your grade");
+
+if (grade < 0 || grade > 100) {
+  console.log("Please enter a number between 0 and 100");
+} 
+else if (grade >= 80) {
+  console.log("A1!");
+}
+ else if (grade >= 70) {
+  console.log("A");
+}
+ else if (grade >= 60) {
+  console.log("B");
+}
+ else if (grade >= 50) {
+  console.log("C");
+}
+ else {
+  console.log("Fail!!!");
+}
